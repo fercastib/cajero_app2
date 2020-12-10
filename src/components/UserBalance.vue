720af3f1ef09b6e062d19f22f8bca1c3d3d6c01b
Fernanda Castiblanco <fernandacastiblanco95@gmail.com>
2020-12-10 15:41:37 -0500

    version 3

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -18,7 +18,7 @@
     created: function(){
         this.username = this.$route.params.username
         let self = this
-        axios.get("https://cajeromintic.herokuapp.com" + this.username)
+        axios.get("https://cajeroapi2020.herokuapp.com/user/balance/" + this.username)
             .then((result) => {
             self.balance = result.data.balance
             })
